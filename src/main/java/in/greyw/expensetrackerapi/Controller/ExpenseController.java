25818efa8e66a927f1330140963051700d1bd77d
greyw <greywhitt@gmail.com>
2022-06-18 18:10:40 -0500

    added routes and methods for filtering expenses

--- src/main/java/in/greyw/expensetrackerapi/Controller/ExpenseController.java
+++ src/main/java/in/greyw/expensetrackerapi/Controller/ExpenseController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -46,6 +47,24 @@ public class ExpenseController {
     public Expense updateExpenseDetails(@RequestBody Expense expense, @PathVariable Long id) {
 
         return expenseService.updateExpenseDetails(id, expense);
+    }
+
+    @GetMapping("/expenses/category")
+    public List<Expense> getExpensesByCategory(@RequestParam String category, Pageable page) {
+
+        return expenseService.readByCategory(category, page);
+    }
+
+    @GetMapping("/expenses/name")
+    public List<Expense> getExpensesByName(@RequestParam String keyword, Pageable page) {
+
+        return expenseService.readByName(keyword, page);
+    }
+
+    @GetMapping("/expenses/date")
+    public List<Expense> getExpensesByDates(@RequestParam(required = false) Date startDate, @RequestParam(required = false) Date endDate, Pageable page) {
+
+        return expenseService.readByDate(startDate, endDate, page);
     }
 
 }
--- src/main/java/in/greyw/expensetrackerapi/Service/ExpenseService.java
+++ src/main/java/in/greyw/expensetrackerapi/Service/ExpenseService.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.List;
 
 @Service
@@ -18,4 +19,10 @@ public interface ExpenseService {
     Expense saveExpenseDetails(Expense expense);
 
     Expense updateExpenseDetails(Long id, Expense expense);
+
+    List<Expense> readByCategory(String category, Pageable page);
+
+    List<Expense> readByName(String keyword, Pageable page);
+
+    List<Expense> readByDate(Date startDate, Date endDate, Pageable page);
 }
