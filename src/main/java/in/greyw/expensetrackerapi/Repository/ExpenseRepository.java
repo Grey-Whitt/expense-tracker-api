490de2e8a8c762f97a6b7cc1bbcb0c58bcf8f317
greyw <greywhitt@gmail.com>
2022-06-20 13:31:35 -0500

    updated finder methods for logged in users

--- src/main/java/in/greyw/expensetrackerapi/Repository/ExpenseRepository.java
+++ src/main/java/in/greyw/expensetrackerapi/Repository/ExpenseRepository.java
@@ -12,16 +12,14 @@ import java.util.Optional;
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
-    // SELECT * FROM tbl_expenses WHERE category=?
-    Page<Expense> findByCategory(String category, Pageable page);
+    Page<Expense> findByUserIdAndCategory(Long userId, String category, Pageable page);
 
-    // SELECT * FROM tbl_expenses WHERE name LIKE '%keyword%'
-    Page<Expense> findByNameContaining(String keyword, Pageable page);
+    Page<Expense> findByUserIdAndNameContaining(Long userId, String keyword, Pageable page);
 
-    // SELECT * FROM tbl_expenses WHERE date BETWEEN 'startDate' AND 'endDate'
-    Page<Expense> findByDateBetween(Date startDate, Date endDate, Pageable page);
+    Page<Expense> findByUserIdAndDateBetween(Long userId, Date startDate, Date endDate, Pageable page);
 
     Page<Expense> findByUserId(Long userId, Pageable page);
 
     Optional<Expense> findByUserIdAndId(Long userId, Long expenseId);
+
 }
